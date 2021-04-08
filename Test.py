ec10e5cc2ac11f1662b22f2cd1ebf45d2058bf57
Plaligraphy <evanac21@gmail.com>
2021-04-08 12:27:02 -0700

    Added comment for clarification on test script

--- Test.py
+++ Test.py
@@ -3,3 +3,5 @@ owm = OWM('233a80f0949773a2a747fda592388231')
 mgr = owm.weather_manager()
 one_call = mgr.one_call(lat=45.357132, lon=-122.84750)
 print(one_call.forecast_daily[0].temperature('fahrenheit').get('feels_like_morn', None))
+
+# Test Script (Not for Final Production use)
